4785555879a2b7d784029db0904af45f5dddfc4d
theodoro <theodorotheodore@gmail.com>
2021-02-26 22:00:06 -0300

    Now, our program can print the available options

---
+++ script.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("Choose the conversion that you want: \n\n");
+    printf("1 - Joules to calories \n");
+    printf("2 - Calories to joules \n");
+    printf("3 - Calories to Kcal \n");
+    printf("4 - Kcal to calories \n");
+    printf("5 - Joules to Kcal \n");
+    printf("6 - Kcal to joules \n\n");
+    
+    return 0;
+}
