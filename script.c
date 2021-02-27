fd4efd62011953e1f2b76df642a7a293b893e116
theodoro <theodorotheodore@gmail.com>
2021-02-26 22:15:01 -0300

    I create an function that converts calories to joules

--- script.c
+++ script.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+
+// this function converts joules in calories
+int cal2joules(int cal){
+    joules = cal * 4186;
+    printf("\n%d cal = %d joules \n", cal, joules);
+    return 0;
+}
+
+
 int main(){
     while(1){
         printf("Choose the conversion that you want: \n\n");
-        printf("1 - Joules to calories \n");
-        printf("2 - Calories to joules \n");
-        printf("3 - Calories to Kcal \n");
-        printf("4 - Kcal to calories \n");
-        printf("5 - Joules to Kcal \n");
-        printf("6 - Kcal to joules \n\n");
+        printf("1 - Calories to joules \n");
+        printf("2 - Calories to Kcal \n");
+        printf("3 - Kcal to calories \n");
+        printf("4 - Joules to Kcal \n");
+        printf("5 - Kcal to joules \n\n");
 
         printf("What is the operation? ");
         char operationCode = 0;
@@ -18,7 +26,18 @@ int main(){
         if(operationCode < 1 || operationCode > 6){
             printf("Invalid code! Please try again! \n\n");
             continue;
+        }
+
+    /*
+        switch(operationCode){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
         }        
+    */
     }
     
     return 0;
