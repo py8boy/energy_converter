195d0f139f1dd928ba1458b15388ea3971b9a92d
theodoro <theodorotheodore@gmail.com>
2021-02-28 19:58:31 -0300

    I already wrote the comments and I will push the repository in my github

--- script.c
+++ script.c
@@ -12,6 +12,7 @@ int cal2joules(){
     return 0;
 }
 
+
 // this function converts calories in kcal
 int cal2kcal(){
     int cal;
@@ -23,6 +24,7 @@ int cal2kcal(){
     return 0;
 }
 
+
 // this function converts kcal in calories
 int kcal2cal(){
     int kcal;
@@ -33,6 +35,7 @@ int kcal2cal(){
     printf("%d kcal = %d cal \n", kcal, cal);
     return 0;
 }
+
 
 // this function converts joules in calories
 int joules2cal(){
@@ -46,8 +49,13 @@ int joules2cal(){
 }
 
 
+
+// main function, the program in fact runs here
 int main(){
+    // in the line bellow we start an infinit loop that store the code
     while(1){
+
+        // and here we show for our user the available operations
         printf("Choose the conversion that you want: \n\n");
         printf("1 - Calories to joules \n");
         printf("2 - Calories to Kcal \n");
@@ -55,15 +63,20 @@ int main(){
         printf("4 - Joules to calories \n\n");
 
         printf("What is the operation? ");
+        // this variable gets the operation code
         int operationCode = 0;
         scanf("%d", &operationCode);
         printf("\n");
 
-        if(operationCode < 0 || operationCode > 6){
+        // and here we verify if it's in the available range
+        if(operationCode < 1 || operationCode > 6){
+            // if the operation its unvailable the software will ask again the operation code and retry in the loop
             printf("Invalid code! Please try again! \n\n");
             continue;
         }
-        else{        
+        // else true, we can proceed with the code execution
+        else{   
+            // in this switch-case struct we call the function referring to the operation     
             switch(operationCode){
                 case 1:
                     cal2joules();
@@ -79,14 +92,17 @@ int main(){
                     break; 
             }
 
+            // in the bellow lines we ask for the user if wants kill the program execution
             printf("\nDo you want to continue? [0 to quit / 1 to continue] ");
             int quittingFlag;
             scanf("%d", &quittingFlag);
 
+            // killing the software execution
             if(quittingFlag == 0){
                 printf("Okay... Se you later... Quitting... \n");
                 break;
             }
+            // or retring to the loop outset
             else{
                 printf("Ok, returning... \n\n\n");
                 continue;
