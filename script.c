1af1862085520ae8d1b5e91e26d45ea21fd33506
theodoro <theodorotheodore@gmail.com>
2021-02-28 19:42:26 -0300

    I remote some useless functions of our code and all our functionalities are working!

--- script.c
+++ script.c
@@ -2,44 +2,46 @@
 
 
 // this function converts calories in joules
-int cal2joules(double cal){
-    double joules = cal * 4186;
-    printf("\n%f cal = %f joules \n", cal, joules);
+int cal2joules(){
+    int cal;
+    printf("Insert the calorie value: ");
+    scanf("%d", &cal);
+
+    int joules = cal * 4186;
+    printf("%d cal = %d joules \n", cal, joules);
     return 0;
 }
 
 // this function converts calories in kcal
-int cal2kcal(double cal){
-    double kcal = cal * 10000;
-    printf("\n%f cal = %f kcal \n", cal, kcal);
+int cal2kcal(){
+    int cal;
+    printf("Insert the calorie value: ");
+    scanf("%d", &cal);
+
+    int kcal = cal / 1000;
+    printf("%d cal = %d kcal \n", cal, kcal);
     return 0;
 }
 
 // this function converts kcal in calories
-int kcal2cal(double kcal){
-    double cal = kcal / 1000;
-    printf("\n%f kcal = %f cal \n", kcal, cal);
-    return 0;
-}
+int kcal2cal(){
+    int kcal;
+    printf("Insert the kcal value: ");
+    scanf("%d", &kcal);
 
-// this function converts joules in kcal
-int joules2kcal(double joules){
-    double kcal = joules * 0.000239;
-    printf("\n%f joules = %f kcal \n", joules, kcal);
-    return 0;
-}
-
-// this function converts kcal in joules
-int kcal2joules(double kcal){
-    double joules = kcal / 0.000239;
-    printf("\n%f kcal = %f joules \n", kcal, joules);
+    int cal = kcal * 10000.00;
+    printf("%d kcal = %d cal \n", kcal, cal);
     return 0;
 }
 
 // this function converts joules in calories
-int joules2cal(double joules){
-    double cal = joules / 4.189;
-    printf("\n%f joules = %f cal \n", joules, cal);
+int joules2cal(){
+    int joules;
+    printf("Insert the joule value: ");
+    scanf("%d", &joules);
+
+    int cal = joules / 4186;
+    printf("%d joules = %d cal \n", joules, cal);
     return 0;
 }
 
@@ -50,54 +52,29 @@ int main(){
         printf("1 - Calories to joules \n");
         printf("2 - Calories to Kcal \n");
         printf("3 - Kcal to calories \n");
-        printf("4 - Joules to Kcal \n");
-        printf("5 - Kcal to joules \n");
-        printf("6 - Joules to calories \n\n");
+        printf("4 - Joules to calories \n\n");
 
         printf("What is the operation? ");
-        char operationCode = 0;
-        scanf("%c", &operationCode);
-        printf("\n\n");
+        int operationCode = 0;
+        scanf("%d", &operationCode);
+        printf("\n");
 
-        if(operationCode < 1 || operationCode > 6){
+        if(operationCode < 0 || operationCode > 6){
             printf("Invalid code! Please try again! \n\n");
             continue;
         }
-
-    
-        switch(operationCode){
-            case 1:
-                double calories;
-                printf("Insert the calorie value: ");
-                scanf("%f", &calories);
-                cal2joules(calories);
-            case 2:
-                double calories;
-                printf("Insert the calorie value: ");
-                scanf("%f", &calories);
-                cal2kcal(calories);
-            case 3:
-                double kilocalories;
-                printf("Insert the kilocalorie value: ");
-                scanf("%f", &kilocalories);
-                kcal2cal(kilocalories);
-            case 4:
-                double joules;
-                printf("Insert the joule value: ");
-                scanf("%f", &joules);
-                joules2kcal(joules);
-            case 5:
-                double kilocalories;
-                printf("Insert the kilocalorie value: ");
-                scanf("%f", &kilocalories);
-                kcal2joules(kilocalories);
-            case 6:
-                double joules;
-                printf("Insert the joule value: ");
-                scanf("%f", &joules);
-                joules2cal(joules);
-        }        
-    
+        else{        
+            switch(operationCode){
+                case 1:
+                    cal2joules();
+                case 2:
+                    cal2kcal();
+                case 3:
+                    kcal2cal();
+                case 4:
+                    joules2cal();   
+            }
+        }      
     }
     
     return 0;
